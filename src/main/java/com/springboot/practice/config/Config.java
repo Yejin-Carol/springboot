37b91605284a901d80f7aecd732d2640ab09e4dc
Yejin-Carol <yejin.carol.jang@gmail.com>
2021-10-21 18:22:42 +0900

    Config #6 - spring configuration
    기초 설정 
    루트 패키지 이하에 빈 설정

---
+++ src/main/java/com/springboot/practice/config/Config.java
@@ -0,0 +1,10 @@
+package com.springboot.practice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.springboot.practice")
+@Configuration
+public class Config {
+
+}
